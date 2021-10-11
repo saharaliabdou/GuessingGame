c9b2f806ca7c53d571cc46dd7298c2b0d0a29866
Sahar Ali <sahar.aliabdou@gmail.com>
2021-10-11 19:11:24 +0200

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace GuessingGame // A gauessing game where the user can guess a number between 1 ans 10. 
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int counter = 0;
+            bool stopthegame = false;
+
+            int random_number = new Random().Next(1, 10);
+            while (!stopthegame)
+            {
+                Console.WriteLine("Please chose an integer between 1 and 10");
+                counter++;
+                string chosen_number = Console.ReadLine();
+                int chosenInt = int.Parse(chosen_number);
+
+                if (chosenInt < random_number)
+                {
+                    Console.WriteLine($"Your chosen number is smaller than secret number ");
+
+                }
+
+
+                else if (chosenInt > random_number)
+                {
+                    Console.WriteLine($"Your chosen number is larger than secret number.");
+
+                }
+
+
+
+                else if (chosenInt == random_number)
+                {
+                    Console.WriteLine($"Your chosen number is the same as the secret number, which is {random_number}! You made it  {counter} tries!");
+                    Console.WriteLine("Would you like to play again?");
+                    Console.WriteLine("Press Y or y to continue");
+                    continueGame = Console.ReadLine();
+                    if (continueGame != "Y" || continueGame !="y")
+                    
+
+                        stopthegame = true;
+                    }
+                   
+                    
+                }
+                
+            }
+
+            
+
+
+
+        }
+    }
+
+}
